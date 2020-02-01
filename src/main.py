6481424849e1851f4581910daf060a1188c414a6
mocharoto <julian.yi26@gmail.com>
2020-02-01 15:36:27 -0700

    tried to find ways to log in automatically

--- src/main.py
+++ src/main.py
@@ -1,24 +1,21 @@
 from selenium import webdriver
 from time import sleep
 from json import load
-from lxml import etree
-import gnugp
 
-with open("/home/julian/Projects/instagram_b0t/credentials.json.gpg") as f:
+
+with open("/home/julian/Projects/instagram_b0t/credentials.json") as f:
     credentials = load(f)
 
-with open("/home/julian/Projects/instagram_b0t/src/xpath.txt") as f1:
-    print(f1)
+class InstaBot:
+    def __init__(self, username, pw):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://instagram.com")
+        sleep(5)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[2]/p/a").click()
+        sleep(2)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input]").send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]/button/div").click()
+        sleep(4)
 
-
-
-
-# class InstaBot:
-#     def __init__(self, username, pw):
-#         self.driver = webdriver.Chrome()
-#         self.driver.get("https://instagram.com")
-#         sleep(5)
-
-print(credentials)
-
-#InstaBot(credentials["user_id"],credentials["password"])
+InstaBot(credentials["user_id"],credentials["password"])
