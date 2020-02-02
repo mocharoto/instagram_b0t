3cc129c964ab9117d45efb538d6e4ec64ca907c5
mocharoto <julian.yi26@gmail.com>
2020-02-02 00:02:36 -0700

    able to log in to instagram

--- src/main.py
+++ src/main.py
@@ -10,12 +10,14 @@ class InstaBot:
         self.driver = webdriver.Chrome()
         self.username = username
         self.driver.get("https://instagram.com")
-        sleep(5)
+        sleep(2)
         self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]").click()
         sleep(2)
         self.driver.find_element_by_xpath("//input[@class = \"_2hvTZ pexuQ zyHYP\"]").send_keys(username)
         self.driver.find_element_by_xpath("//input[@class=\"_2hvTZ pexuQ zyHYP\"]").send_keys(pw)
         self.driver.find_element_by_xpath("//button[@class = \"sqdOP  L3NKy   y3zKF     \"]").click()
-        sleep(4)
+        sleep(2)
+        self.driver.find_element_by_xpath("//button[@class = \"aOOlW   HoLwm \"]").click()
+        sleep(5)
 
 InstaBot(credentials["user_id"],credentials["password"])
