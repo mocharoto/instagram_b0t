dfae7cc679133d5dff80a09348d6b171c47a4055
mocharoto <julian.yi26@gmail.com>
2020-02-01 23:50:56 -0700

    figured out how to log on

--- src/main.py
+++ src/main.py
@@ -2,20 +2,20 @@ from selenium import webdriver
 from time import sleep
 from json import load
 
-
 with open("/home/julian/Projects/instagram_b0t/credentials.json") as f:
     credentials = load(f)
 
 class InstaBot:
     def __init__(self, username, pw):
         self.driver = webdriver.Chrome()
+        self.username = username
         self.driver.get("https://instagram.com")
         sleep(5)
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[2]/p/a").click()
+        self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]").click()
         sleep(2)
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input]").send_keys(username)
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]/button/div").click()
+        self.driver.find_element_by_xpath("//input[@class = \"_2hvTZ pexuQ zyHYP\"]").send_keys(username)
+        self.driver.find_element_by_xpath("//input[@class=\"_2hvTZ pexuQ zyHYP\"]").send_keys(pw)
+        self.driver.find_element_by_xpath("//button[@class = \"sqdOP  L3NKy   y3zKF     \"]").click()
         sleep(4)
 
 InstaBot(credentials["user_id"],credentials["password"])
